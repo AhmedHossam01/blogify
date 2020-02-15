69f3f258efcea9522f866ca4b430384806fb08dc
AhmedHossam01 <50014916+AhmedHossam01@users.noreply.github.com>
2020-02-15 20:05:06 +0200

    Added: go to next, prev, first, last pages and some code improvements

--- src/components/Blog/PaginationUI.js
+++ src/components/Blog/PaginationUI.js
@@ -2,21 +2,44 @@ import React, { Component } from "react";
 import Pagination from "react-bootstrap/Pagination";
 
 export default class PaginationUI extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  prevPage = () => {
+    if (this.props.currentPage > 0) {
+      return this.props.changePage(this.props.currentPage - 1);
+    }
+  };
+
+  nextPage = () => {
+    if (this.props.currentPage < this.props.pages.length - 1) {
+      return this.props.changePage(this.props.currentPage + 1);
+    }
+  };
+
   render() {
+    const { changePage, currentPage, pages } = this.props;
     return (
       <div>
         <Pagination>
-          {this.props.pages.map(number => {
+          <Pagination.First onClick={() => changePage(0)}></Pagination.First>
+          <Pagination.Prev onClick={this.prevPage}></Pagination.Prev>
+          {pages.map(number => {
             return (
               <Pagination.Item
                 key={number}
-                active={number === this.props.currentPage}
-                onClick={() => this.props.changePage(number)}
+                active={number === currentPage}
+                onClick={() => changePage(number)}
               >
                 {number}
               </Pagination.Item>
             );
           })}
+          <Pagination.Next onClick={this.nextPage}></Pagination.Next>
+          <Pagination.Last
+            onClick={() => changePage(pages.length - 1)}
+          ></Pagination.Last>
         </Pagination>
       </div>
     );
